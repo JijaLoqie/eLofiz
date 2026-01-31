0ed084dae6e0d697598f2bea0f52f77d172ce53c
Dmitry <kraskovskiydm@gmail.com>
2026-01-31 17:06:37 +0300

    feature: some important setups

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,8 @@
 import path from "path";
+import react from "@vitejs/plugin-react"
 
 export default {
-  plugins: [],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
