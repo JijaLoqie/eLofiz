61a92d1ad1a2a5643f933f73b5a8e4cc27e4f64b
Dmitry <kraskovskiydm@gmail.com>
2026-02-27 15:45:41 +0300

    feature: move keybindings to modal context provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,26 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { EntityType, ModalType } from "./types.ts";
 import { appStore } from "@/app/appStore.ts";
 import { ensureElement } from "@/utils";
 import { Provider } from "react-redux";
 import { AppEntry } from "@/app/appEntry.tsx";
-import { toggleItemsList } from "@/slices/ModalSlice.ts";
-
-const openActions: Record<string, () => void> = {
-    "1": () => appStore.dispatch(toggleItemsList(EntityType.WIDGETS)),
-    "2": () => appStore.dispatch(toggleItemsList(EntityType.PRESETS)),
-    "3": () => appStore.dispatch(toggleItemsList(EntityType.STREAMS)),
-}
 
 
-document.body.addEventListener("keypress", (e) => {
-    const handlePress = openActions[e.key];
-    if (handlePress) {
-        handlePress();
-    }
-
-})
 
 const modalEditRoot = ReactDOM.createRoot(ensureElement("#root"));
 modalEditRoot.render(
